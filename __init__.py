28571f7ea3b461b63b1fb4d2763201727971a1cb
SmitMaurya23 <smitmaurya2004@gmail.com>
2023-09-26 19:16:38 +0530

    left_shift

--- __init__.py
+++ __init__.py
@@ -30,6 +30,19 @@ def your_function():
     # write code for your_function
     response = "your_function"
     return response
+    
+@app.route("/left_shift", methods=["POST"])
+def left_shift(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    
+    # write code for your_function
+    left= a << b
+    response = str(left)                                #"sum = " + str(sum)
+    return response
 
 
 if __name__== "__main__":
