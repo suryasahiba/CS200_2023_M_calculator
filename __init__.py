529f092b785d1b6bdf777e53072bc3928ff5b21c
suraj-G862 <surajutt@iitbhilai.ac.in>
2023-09-26 19:10:26 +0530

    g15 merge conflict resolved

--- __init__.py
+++ __init__.py
@@ -68,6 +68,22 @@ def LOGICALAND():
     return response
     
 
+@app.route("/isDiff", methods=["POST"])
+def isDiff(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    # write code for your_function
+    if a == b:
+    	ans=0
+    else:
+    	ans=1
+    response = str(ans)
+    return response
+
+
     
 @app.route("/isDiff", methods=["POST"])
 def isDiff(): 
